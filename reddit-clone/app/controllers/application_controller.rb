2ee592bf38fc4bee1f604ca4d4b2ea6174090fbc
Joey Tsui <joeytsui131@gmail.com>
2022-12-23 14:36:28 -0500

    log out

--- reddit-clone/app/controllers/application_controller.rb
+++ reddit-clone/app/controllers/application_controller.rb
@@ -16,7 +16,8 @@ class ApplicationController < ActionController::Base
     end
 
     def logout!
-        current_user.try(:reset_session_token!)
+        current_user.reset_session_token!
+        
         session[:session_token] = nil
         
         @current_user = nil
--- reddit-clone/app/controllers/sessions_controller.rb
+++ reddit-clone/app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+    before_action :require_log_in, only: [:new, :create]
+    before_action :require_log_out, only: [:destroy]
+
     def new
         @user = User.new
         render :new
