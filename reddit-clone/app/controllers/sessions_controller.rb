31e06e60154a1d9fb755b914ad5ccebe74d10b28
lorenzo <lorenzotijerina@gmail.com>
2022-12-23 10:46:17 -0500

    complete users_controller

---
+++ reddit-clone/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password]) 
+        if @user
+            login!(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = ["Invalid Credentials or whatever"]
+            render :new
+        end
+    end
+
+
+
+end
--- reddit-clone/config/routes.rb
+++ reddit-clone/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show, :create, :new]
+  resource :session, only: [:new, :create, :destroy]
 end
